d2d05577baafbdb8136d852a0ecf5e883288d3a2
Chris Kalmar <christian.kalmar@gmail.com>
2021-12-21 17:09:16 +0100

    increase debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,14 @@ echo $STORYBOOK_PATH
 # env
 # cat /github/workflow/event.json
 
+ls -lah $STORYBOOK_PATH
+
+ls -lah
+ls ./node_modules/
+ls ./node_modules/.bin
+
 STORYBOOK="npm run storybook"
 TEST="npm run test"
+
 
 ./node_modules/.bin/concurrently "${STORYBOOK}" "${TEST}" --success first --kill-others
