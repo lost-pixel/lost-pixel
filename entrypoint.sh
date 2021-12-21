ca84969acf98dec6c0e4b79035b996c125e902ed
Chris Kalmar <christian.kalmar@gmail.com>
2021-12-21 16:58:48 +0100

    apply info on storybook build path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo $INPUT_LOST_PIXEL_URL
-env
-cat /github/workflow/event.json
+echo $STORYBOOK_PATH
+# env
+# cat /github/workflow/event.json
 
 STORYBOOK="npm run storybook"
 TEST="npm run test"
 
-# ./node_modules/.bin/concurrently "${STORYBOOK}" "${TEST}" --success first --kill-others
+./node_modules/.bin/concurrently "${STORYBOOK}" "${TEST}" --success first --kill-others
