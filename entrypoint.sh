832ff12a38f242aa2f472a902fc187b9a1630afe
Chris Kalmar <christian.kalmar@gmail.com>
2021-12-22 13:30:04 +0100

    2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,24 +7,32 @@ echo $INPUT_STORYBOOK_PATH
 
 
 STORYBOOK_PATH=/github/workspace/$INPUT_STORYBOOK_PATH
-ls -lah $STORYBOOK_PATH
+# ls -lah $STORYBOOK_PATH
 
 
-STORYBOOK="npm run storybook"
+STORYBOOK="STORYBOOK_PATH=${STORYBOOK_PATH} npm run storybook"
 TEST="npm run test"
-DEBUG="npm run debug"
-DEBUG3="STORYBOOK_PATH=${STORYBOOK_PATH} npm run debug"
-DEBUG4="STORYBOOK_PATH=/yolo/github/workspace/$INPUT_STORYBOOK_PATH npm run debug"
+DEBUG="STORYBOOK_PATH=${STORYBOOK_PATH} npm run debug"
+# DEBUG3="STORYBOOK_PATH=${STORYBOOK_PATH} npm run debug"
+# DEBUG4="STORYBOOK_PATH=/yolo/github/workspace/$INPUT_STORYBOOK_PATH npm run debug"
 
-echo "DEBUG1:"
-npm run debug
+echo $STORYBOOK_PATH
+
+# echo "DEBUG1:"
+# npm run debug
 
 
 
-cd /app
-ls -lah
+# cd /app
+# ls -lah
 # ls ./node_modules/
 # ls ./node_modules/.bin
 
 
 ./node_modules/.bin/concurrently "${DEBUG}" "${STORYBOOK}" "${TEST}" --success first --kill-others
+
+
+# STORYBOOK_PATH=/github/workspace/$INPUT_STORYBOOK_PATH
+# DEBUG3="STORYBOOK_PATH=${STORYBOOK_PATH} npm run debug"
+# DEBUG4="STORYBOOK_PATH=/yolo/github/workspace/$INPUT_STORYBOOK_PATH npm run debug"
+# ./node_modules/.bin/concurrently  "${DEBUG}" "${DEBUG3}" "${DEBUG4}"
