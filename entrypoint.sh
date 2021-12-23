9f3f3038730f4b6879af065814025a754cb14a36
Chris Kalmar <christian.kalmar@gmail.com>
2021-12-23 16:04:20 +0100

    update paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+WORKSPACE=$PWD
+STORYBOOK_PATH=$WORKSPACE/$INPUT_STORYBOOK_PATH
+
+echo "WORKSPACE=$WORKSPACE"
 echo "INPUT_LOST_PIXEL_URL=$INPUT_LOST_PIXEL_URL"
 echo "INPUT_STORYBOOK_PATH=$INPUT_STORYBOOK_PATH"
-
-STORYBOOK_PATH=/github/workspace/$INPUT_STORYBOOK_PATH
+echo "STORYBOOK_PATH=$STORYBOOK_PATH"
 
 cd /app
 
@@ -11,8 +14,8 @@ cd /app
 --verboseRenderer \
 --requireReference \
 --reactUri file:$STORYBOOK_PATH \
---reference /github/workspace/.loki/reference \
---output /github/workspace/.loki/current \
---difference /github/workspace/.loki/difference \
+--reference $WORKSPACE/.loki/reference \
+--output $WORKSPACE/.loki/current \
+--difference $WORKSPACE/.loki/difference \
 --chromeFlags="--headless --disable-gpu --hide-scrollbars --no-sandbox" \
-update
+test
