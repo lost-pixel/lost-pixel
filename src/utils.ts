9f3d4de761eba6ebd667af83c294ff74a248d9f5
Chris Kalmar <christian.kalmar@gmail.com>
2021-12-27 12:47:09 +0100

    merge diff into utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,29 @@
-import { Changes } from './diff';
 import { readdirSync } from 'fs';
 import { UploadFile } from './upload';
+
+export type Files = {
+  reference: string[];
+  current: string[];
+  difference: string[];
+};
+
+export type Changes = {
+  difference: string[];
+  deletion: string[];
+  addition: string[];
+};
+
+export const getChanges = (files: Files): Changes => {
+  return {
+    difference: files.difference.sort(),
+    deletion: files.reference
+      .filter((file) => !files.current.includes(file))
+      .sort(),
+    addition: files.current
+      .filter((file) => !files.reference.includes(file))
+      .sort(),
+  };
+};
 
 type ExtendFileName = { fileName: string; extension: 'after' | 'before' };
 
