d1f9eb97986015898ded459051b7eb3cf41c6997
Chris Kalmar <christian.kalmar@gmail.com>
2021-12-29 14:52:21 +0100

    update demo text

--- examples/storybook-demo/stories/Page.js
+++ examples/storybook-demo/stories/Page.js
@@ -20,18 +20,21 @@ export const createPage = ({ user, onLogout, onLogin, onCreateAccount }) => {
       process starting with atomic components and ending with pages.
     </p>
     <p>
+      Lorem impsum....
+    </p>
+    <p>
       Render pages with mock data. This makes it easy to build and review page states without
       needing to navigate to them in your app. Here are some handy patterns for managing page data
       in Storybook:
     </p>
     <ul>
       <li>
-        Use a higher-level connected component. Storybook helps you compose such data from the
-        "args" of child component stories
-      </li>
-      <li>
         Assemble data in the page component from your services. You can mock these services out
         using Storybook.
+      </li>
+      <li>
+        Use a higher-level connected component. Storybook helps you compose such data from the
+        "args" of child component stories
       </li>
     </ul>
     <p>
