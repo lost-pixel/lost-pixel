f0424e96f0ac4f542bc65024f703d84df3c9cb46
Chris Kalmar <christian.kalmar@gmail.com>
2022-03-26 23:53:09 +0100

    display progress

--- src/shots/shots.ts
+++ src/shots/shots.ts
@@ -8,22 +8,25 @@ export type ShotItem = {
 };
 
 const takeScreenShot = async (browser: Browser, shotItem: ShotItem) => {
-  log(`Taking screenshot of ${shotItem.id}`);
-
   const page = await browser.newPage();
   await page.goto(shotItem.url);
   await page.waitForLoadState();
   await page.screenshot({ path: shotItem.filePath });
-
-  log(`Screenshot of ${shotItem.id} taken and saved to ${shotItem.filePath}`);
 };
 
 export const takeScreenShots = async (shotItems: ShotItem[]) => {
   const browser = await firefox.launch();
+  const total = shotItems.length;
 
   await Promise.all(
-    shotItems.map(async (shotItem) => {
+    shotItems.map(async (shotItem, index) => {
+      const progress = `${index + 1}/${total}`;
+
+      log(`[${progress}] Taking screenshot of ${shotItem.id}`);
       await takeScreenShot(browser, shotItem);
+      log(
+        `[${progress}] Screenshot of ${shotItem.id} taken and saved to ${shotItem.filePath}`,
+      );
     }),
   );
 
