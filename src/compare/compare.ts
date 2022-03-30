d3d03d17c6bf64f5db7c3b80ba4f6141a8b61a75
Chris Kalmar <christian.kalmar@gmail.com>
2022-03-30 15:26:37 +0200

    compare 2 images and create a difference image

---
+++ src/compare/compare.ts
@@ -0,0 +1,53 @@
+import { readFileSync, writeFileSync } from 'fs';
+import pixelmatch from 'pixelmatch';
+import { PNG } from 'pngjs';
+import { resizeImage } from './utils';
+
+export const compareImages = async (
+  baselineShotPath: string,
+  currentShotPath: string,
+  differenceShotPath: string,
+): Promise<number> => {
+  const baselineImageBuffer = readFileSync(baselineShotPath);
+  const currentImageBuffer = readFileSync(currentShotPath);
+
+  if (baselineImageBuffer.equals(currentImageBuffer)) {
+    return 0;
+  }
+
+  let baselineImage: PNG = PNG.sync.read(baselineImageBuffer);
+  let currentImage: PNG = PNG.sync.read(currentImageBuffer);
+
+  const maxWidth = Math.max(
+    baselineImage.width || 100,
+    currentImage.width || 100,
+  );
+  const maxHeight = Math.max(
+    baselineImage.height || 100,
+    currentImage.height || 100,
+  );
+
+  if (
+    baselineImage.width !== currentImage.width ||
+    baselineImage.height !== currentImage.height
+  ) {
+    baselineImage = resizeImage(baselineImage, maxWidth, maxHeight);
+    currentImage = resizeImage(currentImage, maxWidth, maxHeight);
+  }
+  const differenceImage = new PNG({ width: maxWidth, height: maxHeight });
+
+  const pixelDifference = pixelmatch(
+    baselineImage.data,
+    currentImage.data,
+    differenceImage.data,
+    maxWidth,
+    maxHeight,
+    { threshold: 0 },
+  );
+
+  if (pixelDifference > 0) {
+    writeFileSync(differenceShotPath, PNG.sync.write(differenceImage));
+  }
+
+  return pixelDifference;
+};
