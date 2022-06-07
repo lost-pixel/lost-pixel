9ef54b57045a8235c410677409d9fbb1a50dc677
Chris Kalmar <christian.kalmar@gmail.com>
2022-06-07 13:29:21 +0200

    simplify example setups

--- config-templates/example.lostpixel.config.js
+++ config-templates/example.lostpixel.config.js
@@ -1,18 +1,4 @@
 module.exports = {
   storybookUrl: 'examples/storybook-build/storybook-static',
   generateOnly: true,
-  shotConcurrency: 10,
-  lostPixelProjectId: 'project-xxx',
-  ciBuildId: process.env.GITHUB_RUN_ID,
-  ciBuildNumber: process.env.GITHUB_RUN_NUMBER,
-  repository: process.env.REPOSITORY,
-  commitRef: process.env.COMMIT_REF,
-  commitRefName: process.env.COMMIT_REF,
-  commitHash: process.env.COMMIT_HASH,
-  s3: {
-    endPoint: 's3.amazonaws.com',
-    accessKey: 'xxx',
-    secretKey: 'xxxxx',
-    bucketName: 'lostpixel-demo',
-  },
 };
