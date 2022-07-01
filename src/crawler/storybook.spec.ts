bcbb494d56d09ca83aa6f2d03f223d6fe5e38af0
d-ivashchuk <dimaiv@me.com>
2022-07-01 15:25:01 +0200

    fix tests

--- src/crawler/storybook.spec.ts
+++ src/crawler/storybook.spec.ts
@@ -4,7 +4,7 @@ import { defaultTestConfig } from '../testUtils';
 import { collectStories, getIframeUrl, getStoryBookUrl } from './storybook';
 
 const storyBookUrl = getStoryBookUrl(
-  'examples/storybook-demo/storybook-static',
+  'examples/examples/lost-pixel-example-barebone/storybook-static',
 );
 
 beforeAll(async () => {
