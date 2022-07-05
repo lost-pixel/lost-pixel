d28d4c24b4c1eef6309bbc8c580519bda1381fea
Chris Kalmar <christian.kalmar@gmail.com>
2022-07-06 00:31:37 +0200

    update config templates

--- config-templates/example.lostpixel.config.ts
+++ config-templates/example.lostpixel.config.ts
@@ -1,6 +1,8 @@
 import { CustomProjectConfig } from '../src/config';
 
 export const config: CustomProjectConfig = {
-  storybookUrl: 'examples/storybook-build/storybook-static',
+  storybookShots: {
+    storybookUrl: 'examples/storybook-build/storybook-static',
+  },
   generateOnly: true,
 };
