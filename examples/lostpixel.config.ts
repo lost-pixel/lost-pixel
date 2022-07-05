137128126a07b3ee9a9276976731c456394173f4
Chris Kalmar <christian.kalmar@gmail.com>
2022-07-05 23:48:50 +0200

    add new config to cover both modes at the same time

---
+++ examples/lostpixel.config.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+
+import { CustomProjectConfig } from 'lost-pixel';
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: './lost-pixel-example-barebone/storybook-static',
+  },
+  pageShots: {
+    pages: [
+      { path:'/app',name:'app' },
+      { path:'/my-app',name:'my-app' },
+      { path:'/my-app?name=App',name:'my-app-with-query-param' },
+      { path:'/fetch',name:'fetch-static-props' },
+      { path:'/client-fetch',name:'fetch-client' },
+    ],
+    pageBaselineUrl:'http://localhost:3000',
+  },
+  generateOnly: true,
+  failOnDifference: true,
+};
