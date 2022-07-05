bfcebf52436cd874efb8c8144b43c1a1e2183e0f
Chris Kalmar <christian.kalmar@gmail.com>
2022-07-05 20:58:17 +0200

    update gitignore and add config for example project

---
+++ examples/lost-pixel-example-barebone/lostpixel.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  storybookUrl: 'storybook-static',
+  generateOnly: true,
+  failOnDifference: true,
+
+  imagePathBaseline: '.lostpixel/baseline/',
+  imagePathCurrent: '.lostpixel/current/',
+  imagePathDifference: '.lostpixel/difference/',
+};
