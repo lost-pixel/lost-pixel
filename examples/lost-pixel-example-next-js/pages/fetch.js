8a619d5ba0423b4ecea8f51de444c8898b1b1638
Chris Kalmar <christian.kalmar@gmail.com>
2022-07-11 14:36:27 +0200

    remove console logs

--- examples/lost-pixel-example-next-js/pages/fetch.js
+++ examples/lost-pixel-example-next-js/pages/fetch.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home( { data } ) {
-  console.log(data)
 
   return (
     <div className={styles.container}>
