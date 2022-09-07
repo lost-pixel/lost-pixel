3cd1865ff337fd16c1ea4899b1a46c4cdd8207a4
d-ivashchuk <dimaiv@me.com>
2022-09-07 19:59:44 +0200

    add basic telemetry implementation

--- src/runner.ts
+++ src/runner.ts
@@ -7,7 +7,9 @@ import {
   createShotsFolders,
   getEventData,
   isUpdateMode,
+  parseHrtimeToSeconds,
   removeFilesInFolder,
+  sendTelemetryData,
 } from './utils';
 import { config, configure } from './config';
 import { sendResultToAPI } from './upload';
@@ -15,6 +17,7 @@ import { sendInitToAPI } from './sendInit';
 import { log } from './log';
 
 export const runner = async () => {
+  const executionStart = process.hrtime();
   await configure();
   log('Successfully loaded the configuration!');
   try {
@@ -36,17 +39,23 @@ export const runner = async () => {
     }
 
     log('Creating shot folders');
+    const createShotsStart = process.hrtime();
     createShotsFolders();
 
     log('Creating shots');
     const shotItems = await createShots();
 
+    const createShotsStop = process.hrtime(createShotsStart);
+    log(`Creating shots took:${parseHrtimeToSeconds(createShotsStop)}`);
+
     if (config.generateOnly && shotItems.length === 0) {
       log(`Exiting process with nothing to compare.`);
+      log(`Process took ${parseHrtimeToSeconds(createShotsStop)} seconds`);
       process.exit(1);
     }
 
     log('Checking differences');
+    const checkDifferenceStart = process.hrtime();
     const { differenceCount, noBaselinesCount } = await checkDifferences(
       shotItems,
     );
@@ -66,15 +75,51 @@ export const runner = async () => {
       process.exit(1);
     }
 
-    if (!config.generateOnly) {
+    const checkDifferenceStop = process.hrtime(checkDifferenceStart);
+    log(
+      `Checking differences took ${parseHrtimeToSeconds(
+        checkDifferenceStop,
+      )} seconds`,
+    );
+
+    const executionStop = process.hrtime(executionStart);
+
+    log(`Lost Pixel run took ${parseHrtimeToSeconds(executionStop)} seconds`);
+
+    if (
+      config.generateOnly &&
+      process.env.LOST_PIXEL_DISABLE_TELEMETRY !== '1'
+    ) {
+      sendTelemetryData({
+        shotsNumber: shotItems.length,
+        runDuration: Number(parseHrtimeToSeconds(executionStop)),
+      });
+    } else {
       const comparisons = await collect();
       await sendResultToAPI({
         success: true,
         comparisons,
         event: getEventData(config.eventFilePath),
+        durations: {
+          runDuration: Number(parseHrtimeToSeconds(executionStop)),
+          differenceComparisonsDuration: Number(
+            parseHrtimeToSeconds(checkDifferenceStop),
+          ),
+          shotsCreationDuration: Number(
+            parseHrtimeToSeconds(checkDifferenceStop),
+          ),
+        },
       });
     }
   } catch (error: unknown) {
+    const executionStop = process.hrtime(executionStart);
+    if (config.generateOnly) {
+      sendTelemetryData({
+        runDuration: Number(parseHrtimeToSeconds(executionStop)),
+        error,
+      });
+    }
+
     if (error instanceof Error) {
       log(error.message);
     } else {
