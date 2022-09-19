6fbb25f903c9f8b11d172880a5421b7503d4114a
Chris Kalmar <christian.kalmar@gmail.com>
2022-09-20 01:39:29 +0200

    bring more structure to logger

--- src/log.ts
+++ src/log.ts
@@ -1,13 +1,76 @@
-type LogMemory = Array<{
+type LogEntry = {
   timestamp: Date;
+  uniqueItemId?: string;
+  source: 'process' | 'browser';
+  type: 'info' | 'console' | 'network' | 'timeout' | 'error';
   content: unknown[];
-}>;
+};
+
+type LogMemory = Array<LogEntry>;
 
 export const logMemory: LogMemory = [];
 
-export const log = (...content: unknown[]) => {
-  logMemory.push({ timestamp: new Date(), content });
+const renderLog = (entry: LogEntry) => {
   const { log } = console;
 
-  log(...content);
+  log(`[${entry.type}] {${entry.uniqueItemId}} `, ...entry.content);
+};
+
+export const log = {
+  item: {
+    process: (
+      uniqueItemId: LogEntry['uniqueItemId'],
+      type: LogEntry['type'],
+      ...content: unknown[]
+    ) => {
+      const entry: LogEntry = {
+        timestamp: new Date(),
+        uniqueItemId,
+        source: 'process',
+        type,
+        content,
+      };
+
+      logMemory.push(entry);
+      renderLog(entry);
+    },
+    browser: (
+      uniqueItemId: LogEntry['uniqueItemId'],
+      type: LogEntry['type'],
+      ...content: unknown[]
+    ) => {
+      const entry: LogEntry = {
+        timestamp: new Date(),
+        uniqueItemId,
+        source: 'browser',
+        type,
+        content,
+      };
+
+      logMemory.push(entry);
+      renderLog(entry);
+    },
+  },
+  process: (type: LogEntry['type'], ...content: unknown[]) => {
+    const entry: LogEntry = {
+      timestamp: new Date(),
+      source: 'process',
+      type,
+      content,
+    };
+
+    logMemory.push(entry);
+    renderLog(entry);
+  },
+  browser: (type: LogEntry['type'], ...content: unknown[]) => {
+    const entry: LogEntry = {
+      timestamp: new Date(),
+      source: 'browser',
+      type,
+      content,
+    };
+
+    logMemory.push(entry);
+    renderLog(entry);
+  },
 };
