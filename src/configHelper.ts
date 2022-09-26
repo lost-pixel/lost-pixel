272f6a31227d353d067aa7416b89f87f0f428699
David Madner <david.madner@gmail.com>
2022-09-26 17:46:43 +0200

    remove comment

--- src/configHelper.ts
+++ src/configHelper.ts
@@ -9,19 +9,3 @@ export const loadTSProjectConfigFile = async (
 
   return mod?.default ?? mod?.config;
 };
-
-// Things that I have tried
-// leveraging moduleTypes like
-/*
-moduleTypes: {
-  [baseName]: "cjs"
-}
-*/
-// worked for typescript but not for js config files
-
-// Vite load like this
-// https://github.com/vitejs/vite/blob/9acb8391574350389e60b6639db29e042dfc9a67/packages/vite/src/node/config.ts#L915
-// creates tmp file for esm: https://github.com/vitejs/vite/blob/9acb8391574350389e60b6639db29e042dfc9a67/packages/vite/src/node/config.ts#L1051
-// and tests like this
-// https://github.com/vitejs/vite/blob/9acb8391574350389e60b6639db29e042dfc9a67/playground/resolve-config/__tests__/resolve-config.spec.ts#L9
-//
