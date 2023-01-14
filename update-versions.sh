fa05250665c3420ced0c6949c54c22e92cf8f9b8
Chris Kalmar <christian.kalmar@gmail.com>
2023-01-14 18:28:50 +0100

    improve version updater

--- update-versions.sh
+++ update-versions.sh
@@ -1,3 +1,8 @@
-find docs README.md -name '*.md' -exec sed -i '' "s/lost-pixel\/lost-pixel@v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/lost-pixel\/lost-pixel@v$npm_package_version/g" {} +
-find examples -name 'ci.yml' -exec sed -i '' "s/lost-pixel\/lost-pixel@v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/lost-pixel\/lost-pixel@v$npm_package_version/g" {} +
-git add docs README.md
+ACTION_PATTERN="s/lost-pixel\/lost-pixel@v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/lost-pixel\/lost-pixel@v$npm_package_version/g"
+DOCKER_PATTERN="s/lostpixel\/lost-pixel\:v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/lostpixel\/lost-pixel\:v$npm_package_version/g"
+
+find docs README.md -name '*.md' -exec sed -i '' $ACTION_PATTERN {} +
+find examples -name 'ci.yml' -exec sed -i '' $ACTION_PATTERN {} +
+find action.yml -exec sed -i '' $DOCKER_PATTERN {} +
+
+git add docs README.md examples action.yml
