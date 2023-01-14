b9225dd156bffac6ccb85ba7727b8a28d0906a71
Chris Kalmar <christian.kalmar@gmail.com>
2023-01-14 16:51:56 +0100

    improve doc and examples version updater

--- update-versions.sh
+++ update-versions.sh
@@ -1,2 +1,3 @@
 find docs README.md -name '*.md' -exec sed -i '' "s/lost-pixel\/lost-pixel@v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/lost-pixel\/lost-pixel@v$npm_package_version/g" {} +
+find examples -name 'ci.yml' -exec sed -i '' "s/lost-pixel\/lost-pixel@v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/lost-pixel\/lost-pixel@v$npm_package_version/g" {} +
 git add docs README.md
