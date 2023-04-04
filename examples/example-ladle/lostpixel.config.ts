7262f8a52162574a22cae5e88a1dc47e46a3c73a
d-ivashchuk <dimaiv@me.com>
2023-04-04 09:13:37 +0200

    add breakpoint shots for ladle

--- examples/example-ladle/lostpixel.config.ts
+++ examples/example-ladle/lostpixel.config.ts
@@ -5,6 +5,7 @@ import { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
   ladleShots: {
     ladleUrl: 'http://localhost:61000',
+    breakpoints: [368, 1024],
   },
   generateOnly: true,
   failOnDifference: true,
