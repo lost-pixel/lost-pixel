70a8b15bc0edb66986967ee0da33e7d8c984642c
Chris Kalmar <christian.kalmar@gmail.com>
2023-04-05 12:05:08 +0200

    update logger usage

--- src/docker-runner/index.ts
+++ src/docker-runner/index.ts
@@ -4,15 +4,19 @@ import { executeDockerRun } from './utils';
 
 export const runInDocker = async () => {
   const version = getVersion();
+
   if (version) {
-    log(`Running in docker: lost-pixel:${version}`);
+    log.process('info', 'general', `Running in docker: lost-pixel:${version}`);
+
     try {
       await executeDockerRun({ version });
     } catch (error: unknown) {
-      log(error);
+      log.process('error', 'general', error);
     }
   } else {
-    log(
+    log.process(
+      'error',
+      'config',
       'Seems like lost-pixel is missing in your package.json. Running lost-pixel@latest',
     );
   }
