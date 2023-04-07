63742e18f1162015b6e9e8b132d98d010663cda0
Chris Kalmar <christian.kalmar@gmail.com>
2023-04-07 23:31:10 +0200

    bring back original config

--- examples/example-storybook-v6.5-storystore-v7/lostpixel.config.ts
+++ examples/example-storybook-v6.5-storystore-v7/lostpixel.config.ts
@@ -4,9 +4,8 @@ import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
   storybookShots: {
-    storybookUrl: './examples/example-storybook-v6.5-storystore-v7/storybook-static',
+    storybookUrl: './storybook-static',
   },
   generateOnly: true,
   failOnDifference: true,
-  browser:'firefox'
 };
