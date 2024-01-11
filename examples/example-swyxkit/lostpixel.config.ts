875726f7aff6a07da28502f35792bfeb7508390d
d-ivashchuk <dimaiv@me.com>
2024-01-11 13:34:21 +0100

    try bigger threshold for histoire

--- examples/example-swyxkit/lostpixel.config.ts
+++ examples/example-swyxkit/lostpixel.config.ts
@@ -4,7 +4,9 @@ import { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
   histoireShots:{
     histoireUrl: './.histoire/dist',
+
   },
+  threshold:0.04,
   generateOnly: true,
   failOnDifference: true,
 };
