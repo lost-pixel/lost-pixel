c67f9b2a664c9ca2decb8b2773e0fe7ed524d862
Chris Kalmar <christian.kalmar@gmail.com>
2024-02-26 19:14:29 +0100

    filter disabled ladle stories out

--- src/crawler/ladleScreenshots.ts
+++ src/crawler/ladleScreenshots.ts
@@ -23,50 +23,25 @@ export const generateLadleShotItems = (
 ): ShotItem[] => {
   const ladleUrl = isLocalServer ? `${baseUrl}/index.html` : baseUrl;
 
-  return ladleStories.flatMap((ladleStory): ShotItem[] => {
-    const shotName =
-      config.shotNameGenerator?.({ ...ladleStory, shotMode: 'ladle' }) ??
-      ladleStory.id;
-    let label = generateLabel({ browser });
-    let fileNameWithExt = `${shotName}${label}.png`;
+  return ladleStories
+    .filter((story) => story.parameters?.lostpixel?.disable !== true)
+    .filter((story) =>
+      config.filterShot
+        ? config.filterShot({ ...story, shotMode: 'ladle' })
+        : true,
+    )
+    .flatMap((ladleStory): ShotItem[] => {
+      const shotName =
+        config.shotNameGenerator?.({ ...ladleStory, shotMode: 'ladle' }) ??
+        ladleStory.id;
+      let label = generateLabel({ browser });
+      let fileNameWithExt = `${shotName}${label}.png`;
 
-    const shotItem: ShotItem = {
-      shotMode: 'ladle',
-      id: `${ladleStory.story}${label}`,
-      shotName: `${shotName}${label}`,
-      url: `${ladleUrl}?story=${ladleStory.story}&mode=preview`,
-      filePathBaseline: isPlatformModeConfig(config)
-        ? notSupported
-        : path.join(config.imagePathBaseline, fileNameWithExt),
-      filePathCurrent: path.join(config.imagePathCurrent, fileNameWithExt),
-      filePathDifference: isPlatformModeConfig(config)
-        ? notSupported
-        : path.join(config.imagePathDifference, fileNameWithExt),
-      threshold: config.threshold,
-      mask: mask ?? [],
-    };
-
-    const breakpoints = selectBreakpoints(
-      config.breakpoints,
-      modeBreakpoints,
-      ladleStory.parameters?.lostpixel?.breakpoints,
-    );
-
-    if (breakpoints.length === 0) {
-      return [shotItem];
-    }
-
-    return breakpoints.map((breakpoint) => {
-      label = generateLabel({ breakpoint, browser });
-      fileNameWithExt = `${shotName}${label}.png`;
-
-      return {
-        ...shotItem,
+      const shotItem: ShotItem = {
+        shotMode: 'ladle',
         id: `${ladleStory.story}${label}`,
-        shotName: `${ladleStory.story}${label}`,
-        breakpoint,
-        breakpointGroup: ladleStory.story,
-        url: `${ladleUrl}?story=${ladleStory.story}&mode=preview&width=${breakpoint}`,
+        shotName: `${shotName}${label}`,
+        url: `${ladleUrl}?story=${ladleStory.story}&mode=preview`,
         filePathBaseline: isPlatformModeConfig(config)
           ? notSupported
           : path.join(config.imagePathBaseline, fileNameWithExt),
@@ -74,10 +49,42 @@ export const generateLadleShotItems = (
         filePathDifference: isPlatformModeConfig(config)
           ? notSupported
           : path.join(config.imagePathDifference, fileNameWithExt),
-        viewport: { width: breakpoint },
+        threshold: config.threshold,
+        mask: mask ?? [],
       };
+
+      const breakpoints = selectBreakpoints(
+        config.breakpoints,
+        modeBreakpoints,
+        ladleStory.parameters?.lostpixel?.breakpoints,
+      );
+
+      if (breakpoints.length === 0) {
+        return [shotItem];
+      }
+
+      return breakpoints.map((breakpoint) => {
+        label = generateLabel({ breakpoint, browser });
+        fileNameWithExt = `${shotName}${label}.png`;
+
+        return {
+          ...shotItem,
+          id: `${ladleStory.story}${label}`,
+          shotName: `${ladleStory.story}${label}`,
+          breakpoint,
+          breakpointGroup: ladleStory.story,
+          url: `${ladleUrl}?story=${ladleStory.story}&mode=preview&width=${breakpoint}`,
+          filePathBaseline: isPlatformModeConfig(config)
+            ? notSupported
+            : path.join(config.imagePathBaseline, fileNameWithExt),
+          filePathCurrent: path.join(config.imagePathCurrent, fileNameWithExt),
+          filePathDifference: isPlatformModeConfig(config)
+            ? notSupported
+            : path.join(config.imagePathDifference, fileNameWithExt),
+          viewport: { width: breakpoint },
+        };
+      });
     });
-  });
 };
 
 export const collectLadleStories = async (ladleUrl: string) => {
