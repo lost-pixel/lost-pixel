2562f7f8695f8bd293f6bcf7df9da8ae2e42f27d
Henrique Almeida <henrique.bruno.fa@gmail.com>
2024-04-01 10:32:49 +0200

    undo change

--- examples/example-storybook-v8/lostpixel.config.ts
+++ examples/example-storybook-v8/lostpixel.config.ts
@@ -9,12 +9,4 @@ export const config: CustomProjectConfig = {
   },
   generateOnly: true,
   failOnDifference: true,
-
-  compareAfterShot: true,
-  flakynessRetries: 5,
-  // These times are greatly reduced due to compareAfterShot!
-  waitBetweenFlakynessRetries: 500,
-  waitBeforeScreenshot: 0,
-  waitForFirstRequest: 0,
-  waitForLastRequest: 0,
 };
