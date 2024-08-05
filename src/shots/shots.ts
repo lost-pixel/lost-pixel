a584acebbfc323ca0ccd7142f3b5ac17379ba091
Chris Kalmar <christian.kalmar@gmail.com>
2024-08-05 17:25:09 +0200

    check if selector is attached, doesn't have to be visible

--- src/shots/shots.ts
+++ src/shots/shots.ts
@@ -78,6 +78,7 @@ const takeScreenShot = async ({
   if (shotItem.waitForSelector) {
     try {
       await page.waitForSelector(shotItem.waitForSelector, {
+        state: 'attached',
         timeout: config.timeouts.loadState,
       });
     } catch (error: unknown) {
