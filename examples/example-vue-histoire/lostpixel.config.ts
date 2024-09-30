05f4b45d01f16991f1385d67d1e99af737f8673a
Sándor Levcsák <levchak.sanek@gmail.com>
2024-09-30 19:48:06 +0200

    chore: use option afterScreenshot in vue-histoire example

--- examples/example-vue-histoire/lostpixel.config.ts
+++ examples/example-vue-histoire/lostpixel.config.ts
@@ -1,5 +1,9 @@
 import { CustomProjectConfig } from 'lost-pixel';
 
+import sharp from "sharp"
+import fs from "node:fs/promises"
+import path from "node:path"
+
 export const config: CustomProjectConfig = {
   histoireShots:{
     histoireUrl: './.histoire/dist',
@@ -7,4 +11,10 @@ export const config: CustomProjectConfig = {
   threshold: 0.0005,
   generateOnly: true,
   failOnDifference: true,
+  async afterScreenshot (_, { filePathCurrent }) {
+    const { base, dir } = path.parse(filePathCurrent)
+    const tmpShotPath = path.join(dir, `tmp.${base}`)
+    await sharp(filePathCurrent).trim().toFile(tmpShotPath)
+    await fs.rename(tmpShotPath, filePathCurrent)
+  },
 };
